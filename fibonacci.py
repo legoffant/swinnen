6c228e58c43c5b8331ff7eeaba389a4a8cf02533
legoffant <legoff.ant@gmail.com>
2020-10-06 14:42:51 +0200

    ajout de commentaire

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 
-a, b, c = 1, 1, 1
-while c < 11:
-    a, b, c = b, a+b, c+1
+# Suite de fibonacci
+# Chaque terme est egal a la somme des deux termes précédent
+
+a, b, c = 1, 0, 0         # On met à zéro la variable b et le compteur c
+while c < 11:             # tant que c est inférieur a 11 on compte
+    a, b, c = b, a+b, c+1 # Assignation parallèle
     print(b, end=" ")
