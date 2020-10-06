51ca37a65cfcd8a7fe2805c46c5f837ebef719e1
legoffant <legoff.ant@gmail.com>
2020-10-06 15:26:21 +0200

    calcul de volume

---
+++ volumePara.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+# Calcul du volume d'un parallélépipède rectangle
+largeur = 4
+hauteur = 3
+profondeur = 10
+
+# formule générique 
+volume = largeur * hauteur * profondeur
+print("volume = ",volume)
